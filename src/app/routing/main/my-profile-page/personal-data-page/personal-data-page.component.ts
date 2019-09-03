644628cd2a520a8cca47317f5dff8afc2249a153
sswitlik <szczepan.switlik@gmail.com>
2019-09-04 00:10:04 +0200

    styles

--- src/app/routing/main/my-profile-page/personal-data-page/personal-data-page.component.ts
+++ src/app/routing/main/my-profile-page/personal-data-page/personal-data-page.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { ActiveUserService } from '../../../../modules/authorization/active-user.service';
+import { User } from '../../../../entities/user/user.entity';
+import { AutoUnsubscribe } from '../../../../modules/auto-unsubscribe/auto-unsubscribe';
 
 @Component({
   selector: 'app-personal-data-page',
   templateUrl: './personal-data-page.component.html',
-  styles: []
+  styles: [],
 })
-export class PersonalDataPageComponent implements OnInit {
+export class PersonalDataPageComponent extends AutoUnsubscribe implements OnInit {
 
-  constructor() { }
+  user: User;
+
+  constructor(private activeUserService: ActiveUserService) {
+    super();
+  }
 
   ngOnInit() {
+    this.activeUserService.getObservable()
+      .pipe(this.takeUntilDestroy())
+      .subscribe(val => this.user = val);
   }
 
 }
